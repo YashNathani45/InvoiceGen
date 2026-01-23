3a5f8bc8cc5c2ffff26602fc3b92c6b3acc85458
yash <yashnathani455@gmail.com>
2026-01-24 02:53:23 +0530

    fixing vercel issue

--- src/app/api/generate-pdf/route.ts
+++ src/app/api/generate-pdf/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server'
 import puppeteer from 'puppeteer-core'
-import chromium from '@sparticuz/chromium'
+import chromium from '@sparticuz/chromium-min'
 
 export async function POST(req: NextRequest) {
     let browser
